6530f641b4b31b2c207c7991bcc13a59d3b6f4eb
yangshujie <yangshujie@fangcunyisheng.com>
2024-01-18 12:21:13 +0800

    FEAT 使用 numpy 实现感知机的与门

--- logic_circuit/index.py
+++ logic_circuit/index.py
@@ -14,3 +14,28 @@ def AND_v1(x1, x2):
         return True
     else:
         return False    
+
+
+import numpy as np
+def AND_v2(x1, x2):
+    """
+    brief: 与门
+    desc: 使用 numpy 实现感知机的与门
+    x: 输入信号
+    w: 信号权重，代表每个信号的重要程度
+    b: 偏置，代表神经元被激活的容易程度
+
+    数学公式：
+    y = 0 (w1*x1 + w2*x2 <= theta)
+    y = 1 (w1*x1 + w2*x2 > theta)
+    """
+
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.9
+
+    tmp = x*w + b
+    if tmp > 0:
+        return True
+    else:
+        return False
