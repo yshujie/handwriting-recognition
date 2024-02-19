ba22a83bd1e08c0c0f5b1198848c81cb9d382eb6
yshujie <yshujie@163.com>
2024-02-19 09:56:03 +0800

    Feat 新增 mnist_show.py, 实现拉取 mnist 数据,展示图片的功能

---
+++ number_recognition/mnist_show.py
@@ -0,0 +1,23 @@
+import sys, os
+sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
+import numpy as np
+from utils.mnist import load_mnist
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+img = x_train[0]
+label = t_train[0]
+print(label)  # 5
+
+print(img.shape)
+img = img.request(28, 28)
+print(img.shape)
+
+img_show(img)
+
