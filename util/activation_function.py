ba5d1700ffbd1ef302f98c9ec48d8ec825082c46
yshujie <yshujie@163.com>
2024-02-19 09:00:48 +0800

    Feat 新增 indentity_function 激活函数

--- util/activation_function.py
+++ util/activation_function.py
@@ -14,3 +14,19 @@ def sigmoid(x):
             y = 1 / (1 + exp(-x))
     """
     return 1 / (1 + np.exp(-x))
+
+
+def indentity_function(x):
+    """
+        indentity_function(x) -> x
+
+        参数：
+            x: 输入信号
+
+        返回：
+            输入信号
+
+        数学公式：
+            y = x
+    """
+    return x
