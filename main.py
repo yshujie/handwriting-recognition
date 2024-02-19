cad898a5f0ed06470fdcec3085ab9a53635d04f9
yshujie <yshujie@163.com>
2024-02-19 08:47:43 +0800

    Test 测试手写字识别的 init_network(), forward()

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from number_recognition.index import init_network, forward
+
 def main():
-    print("Hello World!")
+    network = init_network()
+    x = np.array([1.0, 0.5])
+    y = forward(network, x)
+    print(y) # [0.31682708 0.69627909]
 
 if __name__ == "__main__":
     main()
