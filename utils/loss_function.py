291065d1b4bf31ccb58ba3a42626482ed4d1d09e
yshujie <yshujie@163.com>
2024-02-23 12:07:06 +0800

    Feat 新增 loss_function.py, 提供均方误差和交叉熵误差计算函数

--- utils/loss_function.py
+++ utils/loss_function.py
@@ -12,3 +12,18 @@ def mean_squared_error(y, t):
         E = 1/2 * Σ(yk - tk)^2
     """
     return 0.5 * np.sum((y-t)**2)
+
+
+def cross_ertropy_error(y, t):
+    """
+        交叉熵误差
+
+        :param y: 神经网络的输出
+        :param t: 监督数据
+        :return: 交叉熵误差
+
+        数学公式：
+        E = -Σ(tk * log(yk))
+    """
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))    
