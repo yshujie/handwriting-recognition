5087db802af43954fc292c29e3a2e797b269e1ba
yshujie <yshujie@163.com>
2024-02-27 18:54:01 +0800

    Fix 更新交叉熵误差函数实现方式,支持 mini-batch 版计算

--- utils/loss_function.py
+++ utils/loss_function.py
@@ -16,14 +16,25 @@ def mean_squared_error(y, t):
 
 def cross_ertropy_error(y, t):
     """
-        交叉熵误差
+        交叉熵误差 (mini-batch 版本)
 
-        :param y: 神经网络的输出
-        :param t: 监督数据
-        :return: 交叉熵误差
+        Args:
+            y: 神经网络的输出
+            t: 监督数据
+
+        Returns:
+            交叉熵误差
 
         数学公式：
-        E = -Σ(tk * log(yk))
+        E = -1/N * ΣΣ(tk * log(yk))
     """
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))    
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    # 监督数据是one-hot-vector的情况下，转换为正确解标签的索引
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
