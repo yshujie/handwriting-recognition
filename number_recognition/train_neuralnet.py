9f829cc1f39392242dc0ecd15428d6a7bb4bc4cf
yshujie <yshujie@163.com>
2024-02-29 14:49:57 +0800

    Fix 更改 TwoLayerNet 的引入方式

--- number_recognition/train_neuralnet.py
+++ number_recognition/train_neuralnet.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
 import numpy as np 
 from utils.mnist import load_mnist
-from number_recognition import TwoLayerNet
+import TwoLayerNet
 
 # 读入数据
 (x_train, t_train), (x_test, t_test) = load_mnist(
