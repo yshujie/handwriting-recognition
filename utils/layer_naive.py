ae31594c43f3f27a78c7d4d563738b83ab3d72f0
yshujie <yshujie@163.com>
2024-03-01 11:57:54 +0800

    Feat layer_naive 实现加法的向前, 向后传播

--- utils/layer_naive.py
+++ utils/layer_naive.py
@@ -43,3 +43,48 @@ class MulLayer:
         dy = dout * self.x
 
         return dx, dy
+
+
+
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        """ 
+        加法的前向传播
+
+        Args:
+            x: 输入数据
+            y: 输入数据
+
+        Returns:
+            out: 输出数据
+
+        数学公式：
+            out = x + y
+        """
+        out = x + y 
+
+        return out
+
+    def backward(self, dout):
+        """
+        加法的反向传播
+
+        Args:
+            dout: 输出数据的导数
+
+        Returns:
+            dx: 输入数据x的导数
+            dy: 输入数据y的导数
+
+        数学公式：
+            dx = dout * 1
+            dy = dout * 1
+        """
+        dx = dout * 1
+        dy = dout * 1
+
+        return dx, dy
