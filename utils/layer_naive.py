423affce1f0d88f62a6e7e695c0033cb1b957595
yshujie <yshujie@163.com>
2024-03-01 11:55:09 +0800

    Feat 新增 layer_naive, 实现乘法向前, 向后传播

---
+++ utils/layer_naive.py
@@ -0,0 +1,45 @@
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        """ 
+        乘法的前向传播
+
+        Args:
+            x: 输入数据
+            y: 输入数据
+
+        Returns:
+            out: 输出数据
+
+        数学公式：
+            out = x * y
+        """
+        self.x = x 
+        self.y = y
+        out = x * y 
+
+        return out 
+    
+    def backward(self, dout):
+        """ 
+        乘法的反向传播
+
+        Args:
+            dout: 输出数据的导数
+
+        Returns:
+            dx: 输入数据x的导数
+            dy: 输入数据y的导数
+
+        数学公式：
+            dx = dout * y
+            dy = dout * x
+        """
+        
+        dx = dout * self.y 
+        dy = dout * self.x
+
+        return dx, dy
